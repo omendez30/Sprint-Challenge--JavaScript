5a7c4c4abfa7b8b8420c8bd670ccb30db51f32ab
omendez30 <omendez9@outlook.com>
2019-01-28 23:11:40 -0500

    fixed bug in classes.js file

--- challenges/classes.js
+++ challenges/classes.js
@@ -22,16 +22,11 @@ class CuboidMaker {
   constructor(attr) {
     this.length = attr.length;
     this.width = attr.width;
-    this.height = attr.heigh;
-  }
-}
-class cuboid extends CuboidMaker {
-  constructor(attr) {
-    super(attr);
+    this.height = attr.height;
   }
 
   volume() {
-    return this.length * this.width * this.length;
+    return this.length * this.width * this.height;
   }
 
   surfaceArea() {
@@ -43,12 +38,12 @@ class cuboid extends CuboidMaker {
     );
   }
 }
-cuboid = {
+
+let cuboid = new CuboidMaker({
   length: 4,
   width: 5,
-  heigh: 5
-};
-
+  height: 5
+});
 // Test your volume and surfaceArea methods by uncommenting the logs below:
 console.log(cuboid.volume()); // 100
 console.log(cuboid.surfaceArea()); // 130
